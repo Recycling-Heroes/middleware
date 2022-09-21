80e9fcd612b33454cb2cdadd324890f281cc4822
jhochsteger <jhochsteger@student.tgm.ac.at>
2022-09-21 12:52:27 +0200

    Project Setup

---
+++ src/test/java/com/example/catew/CatEwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatEwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
